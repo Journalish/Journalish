8e7cee389559a75ba197528671a041685478cccd
Glitch (hello-express) <none>
2020-07-03 01:58:34 +0000

    🐞🍄 Checkpoint ./package.json:8045597/1284 ./server.js:8045597/128 ./handle.js:8045597/80

--- handle.js
+++ handle.js
@@ -1,5 +1,7 @@
-function find (method, id){
-  var id = Math.floor((Math.random() * 1000000) + 1);
+/* */
+
+function find (id){
+  
 }
 function create (){
   var id = Math.floor((Math.random() * 1000000) + 1);
--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.use(express.static("public"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
-
+app.get("/notes", (request, response) => {
+  response.sendFile(__dirname + "/notes.json");
+});
 // send the default array of dreams to the webpage
 app.get("/dreams", (request, response) => {
   // express helps us take JS objects and send them as JSON
