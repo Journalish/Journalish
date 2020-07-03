67cdc3e3890c527f2317b81d4c87bbcee58137f9
Glitch (hello-express) <none>
2020-07-03 02:10:37 +0000

    ⛸☔️ Checkpoint ./handle.js:8045597/509

--- handle.js
+++ handle.js
@@ -1,8 +1,17 @@
-/* */
+/* Do some fancy databasing*/
+let jsoning = require('jsoning');
+let database = new jsoning(__dirname + "/notes.json");
+let all = database.all();
+var db1 = JSON.stringify(all);
+var db = JSON.parse(db1)
 
 function find (id){
-  
+  db[id]
 }
-function create (){
+function create (text){
   var id = Math.floor((Math.random() * 1000000) + 1);
+  database.set(id, text);
+  return id
 }
+console.log(create("lol"))
+
