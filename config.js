5ad9937bbe940943e905a2a74099e3987545c383
Glitch (hello-express) <none>
2020-07-04 22:09:46 +0000

    🍬👶 Checkpoint ./server.js:8045597/1593 ./config.js:8045597/19

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
   brand: "OpenRadio",
-  dburl: ""
+  dburl: "",
+  prefix: "mj"
+  
 };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const config = require("./config.js");
-const Keyv = require('keyv');
-const title = new Keyv('redis://user:pass@localhost:6379', { namespace: 'users' });
-const body = new Keyv('redis://user:pass@localhost:6379', { namespace: 'users' });
-const  date = new Keyv('redis://user:pass@localhost:6379', { namespace: 'users' });
+const Keyv = require("keyv");
+const title = new Keyv(config.dburl, { namespace: config.prefix + "title" });
+const body = new Keyv(config.dburl, { namespace: config.prefix + "body" });
+const date = new Keyv(config.dburl, { namespace: config.prefix + "date" });
 app.disable("x-powered-by");
 
 app.use(express.static("public"));
@@ -13,7 +13,8 @@ app.get("/", (request, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 app.post("/create", (req, res) => {
-  var text = req.query.text
+  var text = req.query.text;
+  var body = req.query.text;
 });
 const listener = app.listen(process.env.PORT, () => {
   console.log("MyJournal is runnning on port " + listener.address().port);
