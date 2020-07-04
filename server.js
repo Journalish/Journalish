779269e9a8df257fc3b4fc944398daafde81bc90
Glitch (hello-express) <none>
2020-07-04 21:46:46 +0000

    💳🕴 Checkpoint ./config.js:8045597/1139 ./server.js:8045597/2273 ./package.json:8045597/1724

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
-app.disable('x-powered-by')
+const config = require("./config.js");
+const Keyv = require('keyv');
+const title = new Keyv('redis://user:pass@localhost:6379', { namespace: 'users' });
+const body = new Keyv('redis://user:pass@localhost:6379', { namespace: 'users' });
+
+app.disable("x-powered-by");
 
 app.use(express.static("public"));
 
-
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", (request, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
-
-
+app.post("/create", (req, res) => {
+  var text = req.query.text
+});
 const listener = app.listen(process.env.PORT, () => {
   console.log("MyJournal is runnning on port " + listener.address().port);
 });
